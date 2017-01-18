ae4d381e72332be8a9e8714a893e0d3783f772f9
agavrilenko <trash@inbox.ru>
2017-01-18 06:28:32 -0500

    Added length and ancestor

--- algorithms2/part2-week1/src/main/java/SAP.java
+++ algorithms2/part2-week1/src/main/java/SAP.java
@@ -1,23 +1,92 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Queue;
+
 
 /**
  * Created by trash on 16-Jan-17.
  */
 public class SAP {
 
+    private Digraph g;
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-
+        g = new Digraph(G);
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w) {
-        return 0;
+    public int length(int s, int y) {
+        boolean[] marked = new boolean[g.V()];
+        int[] length = new int[g.V()];
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(s);
+        marked[s] = true;
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : g.adj(v)) {
+                if (!marked[w]) {
+                    q.enqueue(w);
+                    marked[w] = true;
+                    length[w] = length[v] + 1;
+                    if (y == w) {
+                        return length[w];
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) {
-        return 0;
+
+    public int ancestor(int x, int y) {
+        boolean[] markedX = new boolean[g.V()];
+        int[] lengthX = new int[g.V()];
+        int[] edgeToX = new int[g.V()];
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(x);
+        markedX[x] = true;
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : g.adj(v)) {
+                if (!markedX[w]) {
+                    q.enqueue(w);
+                    markedX[w] = true;
+                    edgeToX[w] = v;
+                    lengthX[w] = lengthX[v] + 1;
+
+                }
+            }
+        }
+        boolean[] markedY = new boolean[g.V()];
+        int[] lengthY = new int[g.V()];
+        int[] edgeToY = new int[g.V()];
+        q = new Queue<>();
+        q.enqueue(y);
+        markedY[y] = true;
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : g.adj(v)) {
+                if (!markedY[w]) {
+                    q.enqueue(w);
+                    markedY[w] = true;
+                    edgeToY[w] = v;
+                    lengthY[w] = lengthY[v] + 1;
+                }
+            }
+        }
+        int aCandidate = -1;
+        int sum = Integer.MAX_VALUE;
+        for (int i = 0; i < g.V(); i++) {
+            if (markedX[i] && markedY[i]) {
+                if (lengthX[i] + lengthY[i] < sum) {
+                    sum = lengthX[i] + lengthY[i];
+                    aCandidate = i;
+                }
+            }
+        }
+        return aCandidate;
+
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -29,6 +98,7 @@ public class SAP {
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
         return 0;
     }
+
 
     // do unit testing of this class
     public static void main(String[] args) {
--- algorithms2/part2-week1/src/test/java/SAPTest.java
+++ algorithms2/part2-week1/src/test/java/SAPTest.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Digraph;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -6,8 +8,55 @@ import org.junit.Test;
 public class SAPTest {
 
     @Test
-    public void testInit(){
+    public void testSimpleLength() {
+        Digraph g = new Digraph(4);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        SAP sap = new SAP(g);
+        Assert.assertEquals(3, sap.length(0, 3));
+        Assert.assertEquals(2, sap.length(0, 2));
+        Assert.assertEquals(-1, sap.length(3, 0));
+    }
 
+    @Test
+    public void testSimpleLength1() {
+        Digraph g = new Digraph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        SAP sap = new SAP(g);
+        Assert.assertEquals(2, sap.length(0, 3));
+        Assert.assertEquals(1, sap.length(0, 2));
+        Assert.assertEquals(-1, sap.length(3, 0));
+    }
+
+    @Test
+    public void testSimpleSAP_Five() {
+        Digraph g = new Digraph(6);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(5, 0);
+        SAP sap = new SAP(g);
+        Assert.assertEquals(5, sap.length(1, 0));
+        Assert.assertEquals(5, sap.ancestor(1, 5));
+    }
+
+    @Test
+    public void testSimpleSAP() {
+        Digraph g = new Digraph(6);
+        g.addEdge(1, 0);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(5, 0);
+        SAP sap = new SAP(g);
+        Assert.assertEquals(1, sap.length(1, 0));
+        Assert.assertEquals(0, sap.ancestor(1, 5));
     }
 
 }
