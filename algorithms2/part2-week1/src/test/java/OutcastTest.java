50ccfc4df92290b7f5669e115d7adf8e2fbbbd09
agavrilenko <trash@inbox.ru>
2017-01-20 18:42:48 -0500

    Tests for outcast

--- algorithms2/part2-week1/src/test/java/OutcastTest.java
+++ algorithms2/part2-week1/src/test/java/OutcastTest.java
@@ -1,6 +1,9 @@
+import edu.princeton.cs.algs4.In;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by trash on 20-Jan-17.
@@ -8,7 +11,15 @@ import static org.junit.Assert.fail;
 public class OutcastTest {
     @Test
     public void testOutcast_SimpleScenrio() {
-        fail();
+        WordNet wordnet = new WordNet("input/synsets.txt", "input/hypernyms.txt");
+        Outcast outcast = new Outcast(wordnet);
+        String[] args = new String[]{"input/outcast5.txt", "input/outcast8.txt", "input/outcast11.txt"};
+        String[] expected = new String[]{"table", "bed", "potato"};
+        for (int t = 0; t < args.length; t++) {
+            In in = new In(new Scanner(OutcastTest.class.getClassLoader().getResourceAsStream(args[t])));
+            String[] nouns = in.readAllStrings();
+            assertEquals(expected[t], outcast.outcast(nouns));
+        }
     }
 
 }
