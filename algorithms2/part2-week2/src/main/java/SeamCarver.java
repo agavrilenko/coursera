ff7e2e8ffdfe827a15cec73b2c5b61a789c6eccd
agavrilenko <trash@inbox.ru>
2017-01-24 00:03:05 -0500

    Added energy function and carve tests

--- algorithms2/part2-week2/src/main/java/SeamCarver.java
+++ algorithms2/part2-week2/src/main/java/SeamCarver.java
@@ -4,8 +4,15 @@ import edu.princeton.cs.algs4.Picture;
  * Created by trash on 22-Jan-17.
  */
 public class SeamCarver {
+    private Picture picture;
+
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if (picture == null) {
+            throw new NullPointerException();
+        }
+        this.picture = new Picture(picture);
+
     }
 
     // current picture
@@ -15,17 +22,39 @@ public class SeamCarver {
 
     // width of current picture
     public int width() {
-        return -1;
+        return picture.width();
     }
 
     // height of current picture
     public int height() {
-        return -1;
+        return picture.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        return -1.0;
+        if (height() <= y || width() <= x) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1) {
+            return 1000;
+        }
+
+        final int rX, gX, bX, rY, gY, bY;
+        final double deltaXSq, deltaYSq;
+
+        rX = picture.get(x + 1, y).getRed() - picture.get(x - 1, y).getRed();
+        gX = picture.get(x + 1, y).getGreen() - picture.get(x - 1, y).getGreen();
+        bX = picture.get(x + 1, y).getBlue() - picture.get(x - 1, y).getBlue();
+        deltaXSq = rX * rX + gX * gX + bX * bX;
+
+        rY = picture.get(x, y + 1).getRed() - picture.get(x, y - 1).getRed();
+        gY = picture.get(x, y + 1).getGreen() - picture.get(x, y - 1).getGreen();
+        bY = picture.get(x, y + 1).getBlue() - picture.get(x, y - 1).getBlue();
+
+        deltaYSq = rY * rY + gY * gY + bY * bY;
+
+        return Math.sqrt(deltaXSq + deltaYSq);
     }
 
     // sequence of indices for horizontal seam
@@ -40,11 +69,23 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        if (seam == null) {
+            throw new NullPointerException();
+        }
+        if (height() <= 1) {
+            throw new IllegalArgumentException();
+        }
 
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        if (seam == null) {
+            throw new NullPointerException();
+        }
+        if (width() <= 1) {
+            throw new IllegalArgumentException();
+        }
 
     }
 }
--- algorithms2/part2-week2/src/test/java/SeamCarverTest.java
+++ algorithms2/part2-week2/src/test/java/SeamCarverTest.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.Picture;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -49,5 +50,41 @@ public class SeamCarverTest {
         carver = new SeamCarver(new Picture(PATH + "1x1.png"));
         int[] seam = new int[]{1};
         carver.removeHorizontalSeam(seam);
+    }
+
+    @Test
+    public void testEnerge_VerticalSeam() {
+        carver = new SeamCarver(new Picture(PATH + "6x5.png"));
+        int[] actual = new int[]{};
+        int[] expeted = new int[]{3, 4, 3, 2, 2};
+        carver.removeVerticalSeam(actual);
+        Assert.assertEquals(expeted, actual);
+
+    }
+
+    @Test
+    public void testEnerge_HorizontalSeam() {
+        carver = new SeamCarver(new Picture(PATH + "6x5.png"));
+        int[] actual = new int[]{};
+        int[] expeted = new int[]{2, 2, 1, 2, 1, 2};
+        carver.removeHorizontalSeam(actual);
+        Assert.assertEquals(expeted, actual);
+    }
+
+    @Test
+    public void testEnerge_Simple() {
+        carver = new SeamCarver(new Picture(PATH + "3x4.png"));
+        Assert.assertEquals(Math.sqrt(52024d), carver.energy(1, 2), 0);
+        Assert.assertEquals(Math.sqrt(52225d), carver.energy(1, 1), 0);
+        Assert.assertEquals(1000, carver.energy(0, 0), 0);
+        Assert.assertEquals(1000, carver.energy(0, 1), 0);
+        Assert.assertEquals(1000, carver.energy(0, 2), 0);
+        Assert.assertEquals(1000, carver.energy(0, 3), 0);
+        Assert.assertEquals(1000, carver.energy(1, 0), 0);
+        Assert.assertEquals(1000, carver.energy(1, 3), 0);
+        Assert.assertEquals(1000, carver.energy(2, 0), 0);
+        Assert.assertEquals(1000, carver.energy(2, 1), 0);
+        Assert.assertEquals(1000, carver.energy(2, 2), 0);
+        Assert.assertEquals(1000, carver.energy(2, 3), 0);
     }
 }
