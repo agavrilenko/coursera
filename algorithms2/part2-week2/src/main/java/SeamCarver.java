a7a81fd6141738df6cd05b5a3bd1b96ad0728348
agavrilenko <trash@inbox.ru>
2017-01-25 23:30:20 -0500

    Added topological sort

--- algorithms2/part2-week2/src/main/java/SeamCarver.java
+++ algorithms2/part2-week2/src/main/java/SeamCarver.java
@@ -1,10 +1,20 @@
 import edu.princeton.cs.algs4.Picture;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by trash on 22-Jan-17.
  */
 public class SeamCarver {
     private Picture picture;
+    private double[][] energy;
+    private int[] vMin;
+    private int[] hMin;
+
+    private Queue<Integer> preorder = new LinkedList<>();
+    private boolean[] marked;
+    private int[] verts;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
@@ -12,12 +22,17 @@ public class SeamCarver {
             throw new NullPointerException();
         }
         this.picture = new Picture(picture);
-
+        energy = new double[this.picture.width()][this.picture.height()];
+        for (int x = 0; x < width() - 1; x++) {
+            for (int y = 0; y < height() - 1; y++) {
+                energy(x, y);
+            }
+        }
     }
 
     // current picture
     public Picture picture() {
-        return null;
+        return picture;
     }
 
     // width of current picture
@@ -54,12 +69,46 @@ public class SeamCarver {
 
         deltaYSq = rY * rY + gY * gY + bY * bY;
 
-        return Math.sqrt(deltaXSq + deltaYSq);
+        energy[x][y] = Math.sqrt(deltaXSq + deltaYSq);
+        return energy[x][y];
     }
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
         return null;
+    }
+
+    Queue<Integer> buildVTopology(int v) {
+        Queue<Integer> postorder = new LinkedList<>();
+        postorder = new LinkedList<>();
+        preorder = new LinkedList<>();
+        marked = new boolean[width() * height()];
+        dfs(postorder, v);
+        return postorder;
+    }
+
+    private void dfs(Queue<Integer> postorder, int v) {
+
+        marked[v] = true;
+        int[] next;
+        if (v >= (height() - 1) * width()) {
+            next = new int[]{};
+        } else if (v % width() == 0) {
+            next = new int[]{v + width(), v + width() + 1};
+        } else if ((v + 1) % width() == 0) {
+            next = new int[]{v + width() - 1, v + width()};
+        } else {
+            next = new int[]{v + width() - 1, v + width(), v + width() + 1};
+        }
+
+        for (int w : next) {
+            if (!marked[w]) {
+                dfs(postorder, w);
+            }
+        }
+        postorder.add(v);
+
+
     }
 
     // sequence of indices for vertical seam
@@ -87,5 +136,16 @@ public class SeamCarver {
             throw new IllegalArgumentException();
         }
 
+    }
+
+    private static class Vertex {
+        private int x;
+
+        public Vertex(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        private int y;
     }
 }
--- algorithms2/part2-week2/src/test/java/SeamCarverTest.java
+++ algorithms2/part2-week2/src/test/java/SeamCarverTest.java
@@ -4,6 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Queue;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by trash on 23-Jan-17.
@@ -58,7 +63,7 @@ public class SeamCarverTest {
         int[] actual = new int[]{};
         int[] expeted = new int[]{3, 4, 3, 2, 2};
         carver.removeVerticalSeam(actual);
-        Assert.assertEquals(expeted, actual);
+        assertEquals(expeted, actual);
 
     }
 
@@ -68,23 +73,60 @@ public class SeamCarverTest {
         int[] actual = new int[]{};
         int[] expeted = new int[]{2, 2, 1, 2, 1, 2};
         carver.removeHorizontalSeam(actual);
-        Assert.assertEquals(expeted, actual);
+        assertEquals(expeted, actual);
     }
 
     @Test
     public void testEnerge_Simple() {
         carver = new SeamCarver(new Picture(PATH + "3x4.png"));
-        Assert.assertEquals(Math.sqrt(52024d), carver.energy(1, 2), 0);
-        Assert.assertEquals(Math.sqrt(52225d), carver.energy(1, 1), 0);
-        Assert.assertEquals(1000, carver.energy(0, 0), 0);
-        Assert.assertEquals(1000, carver.energy(0, 1), 0);
-        Assert.assertEquals(1000, carver.energy(0, 2), 0);
-        Assert.assertEquals(1000, carver.energy(0, 3), 0);
-        Assert.assertEquals(1000, carver.energy(1, 0), 0);
-        Assert.assertEquals(1000, carver.energy(1, 3), 0);
-        Assert.assertEquals(1000, carver.energy(2, 0), 0);
-        Assert.assertEquals(1000, carver.energy(2, 1), 0);
-        Assert.assertEquals(1000, carver.energy(2, 2), 0);
-        Assert.assertEquals(1000, carver.energy(2, 3), 0);
+        assertEquals(Math.sqrt(52024d), carver.energy(1, 2), 0);
+        assertEquals(Math.sqrt(52225d), carver.energy(1, 1), 0);
+        assertEquals(1000, carver.energy(0, 0), 0);
+        assertEquals(1000, carver.energy(0, 1), 0);
+        assertEquals(1000, carver.energy(0, 2), 0);
+        assertEquals(1000, carver.energy(0, 3), 0);
+        assertEquals(1000, carver.energy(1, 0), 0);
+        assertEquals(1000, carver.energy(1, 3), 0);
+        assertEquals(1000, carver.energy(2, 0), 0);
+        assertEquals(1000, carver.energy(2, 1), 0);
+        assertEquals(1000, carver.energy(2, 2), 0);
+        assertEquals(1000, carver.energy(2, 3), 0);
+    }
+
+    @Test
+    public void testTopologicalOrder_3x4() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        carver = new SeamCarver(new Picture(PATH + "3x4.png"));
+        Method buildHTopology = SeamCarver.class.getDeclaredMethod("buildVTopology", int.class);
+        Object result = buildHTopology.invoke(carver, 1);
+        Assert.assertTrue(result instanceof Queue);
+        assertEquals(10, ((Queue<Integer>) result).size());
+    }
+
+    @Test
+    public void testTopologicalOrder_6x5() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        carver = new SeamCarver(new Picture(PATH + "6x5.png"));
+        Method buildHTopology = SeamCarver.class.getDeclaredMethod("buildVTopology", int.class);
+        Object result = buildHTopology.invoke(carver, 2);
+        Assert.assertTrue(result instanceof Queue);
+        Queue<Integer> queue = (Queue<Integer>) result;
+        assertEquals(21, queue.size());
+        System.out.println(result.toString());
+        for (int j = 0; j < carver.height(); j++) {
+            for (int i = 0; i < carver.width(); i++) {
+                int index = carver.width() * j + i;
+                if (queue.contains(index)) {
+                    System.out.print(index);
+                } else {
+                    if (index < 12) {
+                        System.out.print("_|");
+                    } else {
+                        System.out.print("_");
+
+                    }
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
