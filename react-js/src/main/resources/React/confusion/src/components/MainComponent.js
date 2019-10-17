9a55363692a41f3b62e08b7ae1f2c6d496938f75
agavrilenko <trash@inbox.ru>
2019-10-16 20:57:17 -0400

    react redux forms. added state to feedback. revisited

--- react-js/src/main/resources/React/confusion/src/components/MainComponent.js
+++ react-js/src/main/resources/React/confusion/src/components/MainComponent.js
@@ -6,6 +6,8 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import {actions} from 'react-redux-form';
+
 
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect } from 'react-redux';
@@ -22,7 +24,8 @@ import {addComment, fetchDishes} from '../redux/ActionCreators';
 
     const mapDispatchToProps = (dispatch) => ({
        addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-       fetchDishes: () => { dispatch(fetchDishes())}
+       fetchDishes: () => { dispatch(fetchDishes())},
+       resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
     });
 
 class Main extends Component {
@@ -67,7 +70,7 @@ render(){
         <Route exact path="/menu" component={()=><Menu dishes={this.props.dishes} />} />
         <Route path = '/menu/:dishId' component = {DishWithId}/>
         <Route path = '/aboutus' component = {()=><About leaders={this.props.leaders} />}/>
-        <Route exact path="/contactus" component={Contact} />
+        <Route exact path="/contactus" component={()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
         <Redirect to='/home' />
       </Switch>
       <Footer />
---
+++ react-js/src/main/resources/React/confusion/src/redux/forms.js
@@ -0,0 +1,9 @@
+export const InitialFeedback = {
+    firstName : '',
+    lastName: '',
+    telnum:'',
+    email:'',
+    agree: '',
+    contactType: 'Tel.',
+    message: ''
+}
