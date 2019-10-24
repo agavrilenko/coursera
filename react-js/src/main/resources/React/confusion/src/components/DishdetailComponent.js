ffc679b92da57aac0b1007e89af2752d55a59157
agavrilenko <trash@inbox.ru>
2019-10-23 20:23:07 -0400

    React animation

--- react-js/src/main/resources/React/confusion/src/components/DishdetailComponent.js
+++ react-js/src/main/resources/React/confusion/src/components/DishdetailComponent.js
@@ -7,6 +7,7 @@ import {Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -82,13 +83,18 @@ const minLength = (len) => (val) => val && (val.length >= len);
     function RenderDish({dish}){
         return(
         <div className="col-12 col-md-5 m-1">
-        <Card>
-           <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-           <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-           </CardBody>
-        </Card>
+        <FadeTransform in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+            <Card>
+               <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+               <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+               </CardBody>
+            </Card>
+        </FadeTransform>
         </div>
 
         );
@@ -102,10 +108,12 @@ const minLength = (len) => (val) => val && (val.length >= len);
     }
 
     const commts = comments.map((post) =>
+                            <Fade in>
                                <div key={post.id} className="m-1">
                                    <li >{post.comment} </li>
                                    <li >--{post.author},  {new Intl.DateTimeFormat('en-US',{year:'numeric', month:'short',day:'2-digit'}).format(new Date(Date.parse(post.date)))} </li>
                                </div>
+                            </Fade>
 
                            );
 
@@ -113,7 +121,9 @@ const minLength = (len) => (val) => val && (val.length >= len);
         <div className="col-12 col-md-5 m-1">
             <h4>Comments</h4>
             <ul className="list-unstyled">
-                {commts}
+                <Stagger in>
+                    {commts}
+                 </Stagger>
             </ul>
              <CommentForm dishId={dishId} postComment={postComment} />
         </div>
